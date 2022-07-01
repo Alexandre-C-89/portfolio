0c1ce2a6c35e7d36c1d0bc9b2a7b3fe29e805fa2
Alexandre <clemencot.alexandre@gmail.com>
2022-07-01 09:36:40 +0200

    Début portfolio

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+const react from "react";
+
+
+const Header = () => {
+    render (
+        <div className="header">
+            <h1>AC</h1>
+            <nav>
+            <ul>
+                <li>Accueil</li>
+                <li>Contact</li>
+            </ul>
+            </nav>
+        </div>
+    );
+};
+
+export default Header;
